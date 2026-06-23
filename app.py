fc3cf3cca2ee0ce7b01a3ca7e625c3e63ada8031
debyamsingh2006-arch <debyamsingh2006@gmail.com>
2026-06-23 11:40:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -157,4 +157,7 @@ Be direct and practical. Format as a numbered list.
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+port = int(os.environ.get('PORT', 5000))
+
+app.run(host='0.0.0.0', port=port)
